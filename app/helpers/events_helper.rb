24036aeec05ca7b59538d546c136c8a2e8cb5eeb
ricardo <ricardotvera.54@gmail.com>
2020-07-24 12:48:19 -0700

    added final dettails

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+  def check_user(current_user, event)
+  if current_user && current_user.name ==  event.creator.name
+    link_to 'Edit', edit_event_path(event) 
+  end 
+end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def check_usere(current, user)
+    if current && current.name ==  user.name
+    link_to 'Edit', edit_user_path(user) 
+    end
+  end
+
+  def check_userd(current, user)
+    if current && current.name ==  user.name
+      link_to 'Destroy', user, method: :delete, data: { confirm: 'Are you sure?' }
+    end
+  end
 end
