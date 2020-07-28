bd88037f4705cb1040cdabe9d8555ce27a9123f7
ricardo <ricardotvera.54@gmail.com>
2020-07-28 10:02:18 -0700

    Update login_spec.rb

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 RSpec.describe 'login and logout', type: :feature do
-let(:user) {User.create(name: 'momotaro')}
-scenario 'login page' do
-  visit '/signin'
-  fill_in 'Name', with: user.name
-  click_button 'Log me in!'
-  sleep(3)
-  expect(page).to have_content('My Events')
-end
-scenario 'logout' do
-  visit '/signin'
-  fill_in 'Name', with: user.name
-  click_button 'Log me in!'
-  sleep(3)
-  click_on 'Log Out'
-  sleep(3)
-  expect(page). to have_content('Private Events')
-end
+  let(:user) {User.create(name: 'momotaro')}
+    scenario 'login page' do
+    visit '/signin'
+    fill_in 'Name', with: user.name
+    click_button 'Log me in!'
+    sleep(3)
+    expect(page).to have_content('My Events')
+  end
+  scenario 'logout' do
+    visit '/signin'
+    fill_in 'Name', with: user.name
+    click_button 'Log me in!'
+    sleep(3)
+    click_on 'Log Out'
+    sleep(3)
+    expect(page). to have_content('Private Events')
+  end
 end
